4fc0bc249b9d544f486297520b224d47e8386c18
Noah Dubitzky <noahdubitzky2744@gmail.com>
2026-01-09 21:07:51 -0500

    console no longer prints entire text of a message

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ async def handler(event):
         print("channel or sender has been blocked by filter rules, skipping message.")
         return  # 🚫 skip: no media download, no POST
 
-    print(timestamp, effective_sender_name, message_text)
+    print(timestamp, effective_sender_name)
 
     # Media (your function)
     media_path = await download_media(event)
