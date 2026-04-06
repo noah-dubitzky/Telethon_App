53256a916f6c8326dd9e23fdaf7b246755b2e19a
Noah Dubitzky <noahdubitzky2744@gmail.com>
2026-04-06 19:55:46 -0400

    addition of mobile side of application with middleware for initial redirection to either desktop or mobile side

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -12,11 +12,11 @@ const filtersRouter = require('./routes/filters');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'desktop')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'desktop', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'middleware', 'index.html'));
 });
+app.use(express.static(path.join(__dirname, 'public', 'desktop')));
 
 app.use('/api/filters', filtersRouter);
 
